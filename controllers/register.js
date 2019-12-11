9f05ae03cd35077a63ea8272bbbb214aa37c8252
Ghislain D <deffpatou@gmail.com>
2019-12-11 04:05:55 -0500

    register form now validates the field values in the request

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,12 @@
-const handleRegister = (req, res, db, bcrypt) => {
+function areInvalid (name, email, password){
+    return (!name || !email || password.length < 8)
+}
 
+
+const handleRegister = (req, res, db, bcrypt) => {
     const { name, email, password } = req.body;
+
+    if(areInvalid(name, email, password)) return res.status(400).json("Invalid Form Submission")
 
     //hash the password and update the database
     bcrypt.hash(password, 10).then(hash => {
