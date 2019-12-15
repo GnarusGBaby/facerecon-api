5b205984424620a7107ffcadcf221cce7cf57394
Ghislain D <deffpatou@gmail.com>
2019-12-15 18:08:38 -0500

    updating master branch after pushing to heroku

--- controllers/newsfeed.js
+++ controllers/newsfeed.js
@@ -3,12 +3,12 @@ const newsapi = new NewsAPI(process.env.NEWSAPI_KEY)
 
 function handleNewsFeed(req, res) {
 
-    newsapi.v2.topHeadlines(req.body).then(response => {
-        console.log(response);
+    newsapi.v2.everything(req.body).then(response => {
+        console.log("response", response.totalResults);
         res.json(response.articles);
     }).catch(err => {
         console.log(err)
-        res.status(400).json("unable to get headlines")
+        res.status(400).json("unable to get news")
     })
 }
 
