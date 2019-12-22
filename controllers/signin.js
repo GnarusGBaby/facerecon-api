f26d1e1167668627009d736110c873ec7a60504f
Ghislain <g.avet.gnaritas@gmail.com>
2019-12-22 00:00:24 -0500

    signin now responds with session data instead of a user

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+
 const handleSignin = (req, res, db, bcrypt) => {
     //get the emails and passwords of every users whose email matches the
     //one signin in...
@@ -23,11 +25,24 @@ const getAuthTokenId = () => {
     console.log('auth ok')
 }
 
+const signToken = (email) => {
+    const jwtPayload = {email};
+    return jwt.sign(jwtPayload, process.env.JWT_SECRET, {expiresIn: "20m"});
+}
+
+const createSessions = (user) => {
+    //create JWT token
+    const token = signToken(user.email);
+    //return user data
+    return { success: "true", userId: user.id, token }
+}
+
 const signinAuthentication = (req, res, db, bcrypt) => {
     const { authorization } = req.headers;
     return authorization ? getAuthTokenId() :
         handleSignin(req, res, db, bcrypt)
-            .then(data => res.json(data))
+            .then(user => createSessions(user))
+            .then(session => res.json(session))
             .catch(err => res.status(400).json(err))
 }
 
